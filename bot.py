00edf9bdf45b59fc36bc5223c864067436f2915c
Cody Receno <cody@nerv.local>
2021-05-12 11:43:36 -0400

    first push

---
+++ bot.py
@@ -0,0 +1,27 @@
+# bot.py
+import os
+
+import discord
+import random
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'valorant?' or message.content == "val?":
+        response = "no"
+        await message.channel.send(response)
+
+client.run(TOKEN)
